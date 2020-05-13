9ac348d3d6f39c80d4e23bfa284b61f1a6db9867
duai <duaifzn@gmail.com>
2020-05-13 09:53:24 +0800

    gcloud deploy

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const flash = require('connect-flash')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
 const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
