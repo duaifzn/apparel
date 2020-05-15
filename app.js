bd35c0f3ee7161c83c9904a7212e7304431264d5
duai <duaifzn@gmail.com>
2020-05-15 16:19:03 +0800

    a

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main', helpers: require('./config/handlebars-helper') }))
 app.set('view engine', 'handlebars')
 
